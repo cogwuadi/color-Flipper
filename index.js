0fed59882ea9870eb969012e17c013b2acbaba33
OGWUADI CHUKWUDI COLLINS <cogwuadi@gmail.com>
2022-08-20 23:26:24 +0100

    this is a flipper colors

---
+++ index.js
@@ -0,0 +1,14 @@
+let colors = ["red", "blue", "yellow", "green", "orange"];
+
+let btn = document.getElementById ("btn");
+let color = document.querySelector ("#color");
+
+btn.addEventListener("click", function () {
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber]
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random () * colors.length);
+}
